0da24dcaed5eee786989eac8a41e826015987574
Michael Yu <Michael.Yu@sportsbet.com.au>
2017-06-24 01:03:04 +1000

    init commit

---
+++ src/main/java/au/com/michaelyu/Application.java
@@ -0,0 +1,15 @@
+package au.com.michaelyu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by michaely on 23/06/2017.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
