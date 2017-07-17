0c90047f2c8e0893160a2b3acd29c147bab14faa
rurutia <haoyumichael@gmail.com>
2017-07-17 23:00:32 +1000

    test s3 readonly

--- src/main/java/au/com/michaelyu/configuration/S3Configuration.java
+++ src/main/java/au/com/michaelyu/configuration/S3Configuration.java
@@ -2,23 +2,28 @@ package au.com.michaelyu.configuration;
 
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class S3Configuration {
 
     @Bean
     public AmazonS3 configureClient() {
+        AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+
 //        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
 //                            .withCredentials(new ProfileCredentialsProvider("default"))
 //                            .build();
 //
-//        List<Bucket> buckets = s3Client.listBuckets();
-//        for (Bucket bucket : buckets) {
-//            System.out.println(bucket.getName() + ": " + bucket.getCreationDate());
-//        }
-//        return s3Client;
-        return null;
+        List<Bucket> buckets = s3Client.listBuckets();
+        for (Bucket bucket : buckets) {
+            System.out.println(bucket.getName() + ": " + bucket.getCreationDate());
+        }
+        return s3Client;
     }
 }
